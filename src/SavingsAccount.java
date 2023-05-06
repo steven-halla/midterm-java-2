6987572ebdb295580bd6b7544c83cc6b9d0d4be5
steven-halla <69405488+steven-halla@users.noreply.github.com>
2023-05-06 15:30:26 -0700

    savings account

--- src/SavingsAccount.java
+++ src/SavingsAccount.java
@@ -1,2 +1,21 @@
-public class SavingsAccount extends BankAccount{
+class SavingAccount extends BankAccount {
+
+    public SavingAccount() {
+        super();
+    }
+
+    public SavingAccount(String customerName, int accountID, double balance) {
+        super(customerName, accountID, balance);
+    }
+
+    @Override
+    public void withdraw(double amount) {
+        if (getBalance() - amount >= 0) {
+            super.withdraw(amount);
+        } else {
+            System.out.println("Transaction declined. Savings account cannot be overdrawn.");
+        }
+    }
 }
+
+// no over draft
