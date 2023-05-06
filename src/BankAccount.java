74314f993f4b96349a40ef734f76746f2eaab110
steven-halla <69405488+steven-halla@users.noreply.github.com>
2023-05-06 16:32:20 -0700

    corrected code to include creating a savings account

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -73,62 +73,8 @@ public class BankAccount {
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        BankAccount account = null;
 
-        while (true) {
-            System.out.println("\nBank Account System\n");
-            System.out.println("1. Create Savings Account");
-            System.out.println("2. Exit");
-            System.out.print("\nEnter your choice: ");
-
-            int choice = scanner.nextInt();
-
-            if (choice == 1) {
-                System.out.print("Please input your first name: ");
-                scanner.nextLine();
-                String firstName = "";
-                while (true) {
-                    firstName = scanner.nextLine();
-                    if (firstName.matches("^[a-zA-Z]*$")) {
-                        break;
-                    }
-                    System.out.print("Invalid input. First name can only contain letters. Please try again: ");
-                }
-
-                System.out.print("Please input your last name: ");
-                String lastName = "";
-                while (true) {
-                    lastName = scanner.nextLine();
-                    if (lastName.matches("^[a-zA-Z]*$")) {
-                        break;
-                    }
-                    System.out.print("Invalid input. Last name can only contain letters. Please try again: ");
-                }
-
-                String customerName = firstName + " " + lastName;
-
-                System.out.print("Enter your account ID: ");
-                int accountID = scanner.nextInt();
-
-                System.out.print("Enter your balance: ");
-                double balance = scanner.nextDouble();
-
-                account = new BankAccount(customerName, accountID, balance);
-
-                System.out.println("\nSavings Account created successfully!");
-                System.out.println("Name: " + customerName);
-                System.out.println("Account ID: " + accountID);
-                System.out.println("Balance: $" + balance);
-                System.out.println("Date Created: " + account.getDateCreated());
-            } else if (choice == 2) {
-                break;
-            } else {
-                System.out.println("\nInvalid choice. Please try again.");
-            }
-        }
     }
-
 
 
 }
