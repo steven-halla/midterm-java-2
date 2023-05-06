512741a8ad46c5a8e7ed82aa1d916c5a472a3169
steven-halla <69405488+steven-halla@users.noreply.github.com>
2023-05-06 16:21:51 -0700

    validations are now complete

--- src/AccountCreator.java
+++ src/AccountCreator.java
@@ -1,64 +1,57 @@
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.Scanner;
 
 public class AccountCreator {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Welcome to the Account Creator!");
-
         String firstName = "";
         String lastName = "";
-
-        while (firstName.isEmpty() || lastName.isEmpty()) {
-            if (firstName.isEmpty()) {
-                System.out.print("Please enter your first name: ");
-                firstName = scanner.nextLine();
-
-                if (!firstName.matches("^[a-zA-Z]+$")) {
-                    System.out.println("Invalid input. First name can only contain letters.");
-                    firstName = "";
-                }
-            } else {
-                System.out.print("Please enter your last name: ");
-                lastName = scanner.nextLine();
-
-                if (!lastName.matches("^[a-zA-Z]+$")) {
-                    System.out.println("Invalid input. Last name can only contain letters.");
-                    lastName = "";
-                }
-            }
-        }
-
-        String name = firstName + " " + lastName;
-
         int accountID = 0;
-        while (accountID == 0) {
-            System.out.print("Please enter your account ID: ");
-            try {
-                accountID = scanner.nextInt();
-            } catch (Exception e) {
-                System.out.println("Invalid input. Account ID can only contain numbers.");
-                scanner.nextLine();
+        double balance = 0.0;
+
+        while (firstName.equals("")) {
+            System.out.print("Please input first name: ");
+            firstName = scanner.nextLine();
+            if (!firstName.matches("[a-zA-Z]+")) {
+                System.out.println("Invalid input. Name can only contain letters. Please try again.");
+                firstName = "";
             }
         }
 
-        double balance = 0;
-        while (balance == 0) {
-            System.out.print("Please enter your balance: ");
-            try {
-                balance = scanner.nextDouble();
-            } catch (Exception e) {
-                System.out.println("Invalid input. Balance can only contain numbers.");
-                scanner.nextLine();
+        while (lastName.equals("")) {
+            System.out.print("Please input last name: ");
+            lastName = scanner.nextLine();
+            if (!lastName.matches("[a-zA-Z]+")) {
+                System.out.println("Invalid input. Name can only contain letters. Please try again.");
+                lastName = "";
             }
         }
 
-        BankAccount account = new BankAccount(name, accountID, balance);
+        while (true) {
+            System.out.print("Please input account ID (4 digits): ");
+            String accountIDStr = scanner.nextLine();
+            if (accountIDStr.matches("\\d{4}")) {
+                accountID = Integer.parseInt(accountIDStr);
+                break;
+            }
+            System.out.println("Invalid input. Account ID must be 4 digits. Please try again.");
+        }
 
-        System.out.println("\nAccount created successfully!");
-        System.out.println("Name: " + name);
+        while (true) {
+            System.out.print("Please input account balance: ");
+            String balanceStr = scanner.nextLine();
+            try {
+                balance = Double.parseDouble(balanceStr);
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Balance must be a number. Please try again.");
+            }
+        }
+
+        String customerName = firstName + " " + lastName;
+        BankAccount account = new BankAccount(customerName, accountID, balance);
+        System.out.println("\nSavings Account created successfully!");
+        System.out.println("Name: " + customerName);
         System.out.println("Account ID: " + accountID);
         System.out.println("Balance: $" + balance);
         System.out.println("Date Created: " + account.getDateCreated());
